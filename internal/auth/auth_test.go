2a3b4bb0b22fe7332a2d2a71bf4c2c33f55bb09b
Cooper Standard <cooper@cooperstandard.org>
2025-09-27 14:30:51 -0700

    add a couple tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,49 @@
+package auth_test
+
+import(
+	"github.com/bootdotdev/learn-cicd-starter/internal/auth"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		headers http.Header
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "functions errors if authorization header is not present",
+			headers: http.Header{},
+			wantErr: true,
+		}, {
+			name:    "function returns authorization header if present",
+			headers: http.Header{
+				"Authorization": {"ApiKey testValue"},
+			},
+			want:    "testValue",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := auth.GetAPIKey(tt.headers)
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("GetAPIKey() failed: %v", gotErr)
+				}
+				return
+			}
+			if tt.wantErr {
+				t.Fatal("GetAPIKey() succeeded unexpectedly")
+			}
+			// TODO: update the condition below to compare got with tt.want.
+			if got != tt.want {
+				t.Errorf("GetAPIKey() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
